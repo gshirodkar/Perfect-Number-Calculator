7c75e9331a0cc108f30cb17692d9f39d1c44d7b0
gshirodkar <coolgauru@gmail.com>
2016-08-16 21:08:52 -0400

    Add files via upload
    Main Code

---
+++ basic.java
@@ -0,0 +1,22 @@
+public class basic {
+	public static boolean perf(double n){
+		double sum= 0;
+		for(double i= 1;i < n;i++){
+			if(n % i == 0){
+				sum+= i;
+			}
+		}
+		return sum == n;
+	}
+	public static void main(String[]args){
+	double count=0;
+		while(true){
+			if(perf(count)==true){
+				System.out.println(count);
+			}
+			count++;
+		}
+	}
+	
+
+}
