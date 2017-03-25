e6c1ec4a0ecf8fb3b0109a83eb9bf71ba8581a21
Gaurav Shirodkar <coolgauru@gmail.com>
2017-03-25 17:26:17 -0400

    Added a easy to use GUI
    The new GUI will allow the user to calculate a specific amount of perfect numbers

--- Perfectnum.java
+++ Perfectnum.java
@@ -1,3 +1,7 @@
+import java.io.*;
+import java.util.*;
+
+import javax.swing.JOptionPane;
 public class basic {
 	public static boolean isperfectnum(double n){
 		double sum= 0;
@@ -9,12 +13,23 @@ public class basic {
 		return sum == n;
 	}
 	public static void main(String[]args){
-	double count=0;
-		while(true){
-			if(perf(count)==true){
-				System.out.println(count);
+		
+		
+		String numberofperfectnums = JOptionPane.showInputDialog("Enter the amount of perfect numbers to calculate. \n WARNING: The digits in perfect numbers increases rapidly.");
+		int pfnums = Integer.parseInt(numberofperfectnums);//pfnums is the number of perfectnms to calculate
+		JOptionPane.showMessageDialog(null,"The program will now calculate the perfect numbers up to the specified amount. \n For large numbers, please be patient as it will take a while.");
+		ArrayList perfectnums = new ArrayList();
+	int perfectnumberlist=0;//counter for number of perfectnums
+		double count=1;
+		while(perfectnumberlist<pfnums){
+			if(isperfectnum(count)==true){// if its a perfect number return true and add it to perfectnums arraylist
+				perfectnums.add(count);
+				perfectnumberlist++;
 			}
 			count++;
+			
 		}
+		
+		JOptionPane.showMessageDialog(null,perfectnums);
 	}
 }
